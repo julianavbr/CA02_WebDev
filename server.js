207668bfd8d6ff4e57cb16f5eb85572766132ab4
Juliana <julianav.br@gmail.com>
2021-04-22 18:11:12 +0100

    V03
    Node.js done

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/api', (req, res) => res.send('Its working!'));
+
+app.listen(process.env.port || 4000, function(){
+    console.log('now listening for requests');
+});
+
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
