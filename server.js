3ab0bc2246e90d0e9b48033ffe5038f93a72c1a6
Juliana <julianav.br@gmail.com>
2021-04-25 16:13:04 +0100

    Integration with old webpage done

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
+var path = require("path");
+
 
 app.get('/api', (req, res) => res.send('Its working!'));
 
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for requests');
+
 });
 
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
 console.log('May Node be with you')
+
+// app.get('/', function(req, res) {
+//     res.send();
+// })
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+// exports.getHome = (req, res) => {
+//     res.send(page);
+// }
+app.get('/', (req, res) => {
+    res.sendFile('C:/Users/Jorge/Google Drive/Education/IT/Semester 06/T01_Interactive Web Application/CA02_WebDev/chocolateShop/views/'+'index.html')
+})
