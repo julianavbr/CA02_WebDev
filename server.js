1fa887ae545100fd324899d17c2004b801d6461e
Juliana <julianav.br@gmail.com>
2021-04-25 17:05:46 +0100

    Integration with old webpage done w/ css

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+
 var path = require("path");
-
-
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static('chocolateShop'));
+app.set('views', './views');
+app.set('view engine', 'html');
 app.get('/api', (req, res) => res.send('Its working!'));
 
 app.listen(process.env.port || 4000, function(){
@@ -24,6 +27,9 @@ console.log('May Node be with you')
 // exports.getHome = (req, res) => {
 //     res.send(page);
 // }
+// app.get('/', (req, res) => {
+//     res.sendFile('C:/Users/Jorge/Google Drive/Education/IT/Semester 06/T01_Interactive Web Application/CA02_WebDev/chocolateShop/views/'+'index.html')
+// })
 app.get('/', (req, res) => {
-    res.sendFile('C:/Users/Jorge/Google Drive/Education/IT/Semester 06/T01_Interactive Web Application/CA02_WebDev/chocolateShop/views/'+'index.html')
+    res.sendFile(__dirname + '/chocolateShop/views/index.html')
 })
