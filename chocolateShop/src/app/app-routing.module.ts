517c43a5566f9f6cf91152794580d65b14886201
Juliana <julianav.br@gmail.com>
2021-04-26 21:16:49 +0100

    Adding angular - 2

--- chocolateShop/src/app/app-routing.module.ts
+++ chocolateShop/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ChocolatesListComponent} from "./chocolates-list/chocolates-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'chocolates-list',
+    component: ChocolatesListComponent,
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- chocolateShop/src/app/app.module.ts
+++ chocolateShop/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+import { ChocolatesComponent } from './chocolates/chocolates.component';
+import { ChocolatesListComponent } from './chocolates-list/chocolates-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HttpClientModule,
+    ChocolatesComponent,
+    ChocolatesListComponent,
   ],
   imports: [
     BrowserModule,
