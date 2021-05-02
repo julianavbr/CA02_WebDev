9baa7dc9056ff823034b45a2fd799fd6580cb1f1
julianavbr <julianav.br@gmail.com>
2021-05-02 21:21:33 +0000

    uri

--- server/database/datainfo.js
+++ server/database/datainfo.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // dbHost: process.env.MONGO_URI,
-    dbHost: 'mongodb+srv://cloudMongo001:ERuPVMnQ7mdDKvn@cluster0.ompkb.mongodb.net/chocolateShop?retryWrites=true&w=majority' ,
+    dbHost: process.env.MONGO_URI,
+    // dbHost: 'mongodb+srv://cloudMongo001:ERuPVMnQ7mdDKvn@cluster0.ompkb.mongodb.net/chocolateShop?retryWrites=true&w=majority' ,
     dbName: 'chocolateShop',
     dbCollection: 'chocolate'
-}
+};
