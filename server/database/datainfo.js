1afa4e85df5184bdd5949ba26806fd2fccfa35e6
julianavbr <julianav.br@gmail.com>
2021-05-02 21:37:38 +0000

    teste03

--- server/database/datainfo.js
+++ server/database/datainfo.js
@@ -1,3 +1,5 @@
+require('dotenv').config({path: __dirname + '.env'});
+
 module.exports = {
     dbHost: process.env.MONGO_URI,
     // dbHost: 'mongodb+srv://cloudMongo001:ERuPVMnQ7mdDKvn@cluster0.ompkb.mongodb.net/chocolateShop?retryWrites=true&w=majority' ,
